75cbba818025dc51407260ceb15aaf0c15afdb55
rodrigo.bulgarelli <rodrigo.bulgarelli@mstech.com.br>
2014-09-02 16:30:27 -0300

    Testes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,14 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
 	$scope.name = "Rodrigo";
+	$scope.email = "";
+        $scope.senha = "";
+        $scope.dataNascimento = "";
+
+        $scope.cadastrar = function(){
+           alert("Usuário " + $scope.email + " cadastrado!");
+           console.log("Usuário " + $scope.senha + " cadastrado!");
+	};
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
