0e4e018062afa543db8c9a994b5d5c2a4f7bdeb9
rodrigo.bulgarelli <rodrigo.bulgarelli@mstech.com.br>
2014-09-02 15:26:38 -0300

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,16 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+	$scope.name = "Rodrigo";
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
